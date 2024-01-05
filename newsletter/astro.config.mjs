1e27b2daf90f1d0ecef32b682eff07744b4b3ca3
Kuba Bujanowicz <bujanowicz_kuba@interia.pl>
2024-01-05 18:49:23 +0100

    :building_construction: (src/): change to yarn package manager

--- newsletter/astro.config.mjs
+++ newsletter/astro.config.mjs
@@ -1,5 +1,6 @@
-import { defineConfig } from "astro/config";
-import qwikdev from '@qwikdev/astro';
+import { defineConfig } from 'astro/config';
+
+import qwikdev from "@qwikdev/astro";
 
 // https://astro.build/config
 export default defineConfig({
