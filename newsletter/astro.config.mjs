ecb6af8c51341bd88545f5cfb6a340f1c2e4bc55
Kuba Bujanowicz <bujanowicz_kuba@interia.pl>
2024-01-05 16:15:54 +0100

    :wrench: (astro.config.mjs): add qwikdev integration to astro.config.mjs

--- newsletter/astro.config.mjs
+++ newsletter/astro.config.mjs
@@ -1,6 +1,7 @@
 import { defineConfig } from "astro/config";
+import qwikdev from '@qwikdev/astro';
 
 // https://astro.build/config
 export default defineConfig({
-  output: "server",
+  integrations: [qwikdev()]
 });
