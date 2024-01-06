46b00c5df2bf64efc0980314276fbf4da04a19b1
Kuba Bujanowicz <bujanowicz_kuba@interia.pl>
2024-01-06 21:16:33 +0100

    :wrench: (astro.config.mjs): add netlify adapter and change output to server

--- newsletter/astro.config.mjs
+++ newsletter/astro.config.mjs
@@ -1,11 +1,13 @@
 import { defineConfig, squooshImageService } from 'astro/config';
-
+import netlify from '@astrojs/netlify';
 import qwikdev from "@qwikdev/astro";
 
 // https://astro.build/config
 export default defineConfig({
+  output: 'server',
   integrations: [qwikdev()],
   image: {
     service: squooshImageService()
-  }
+  },
+  adapter: netlify()
 });
