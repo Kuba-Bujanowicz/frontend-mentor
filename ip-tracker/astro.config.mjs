c634ccada5285ab1b931dc5c9f81472d094f0a55
Kuba-Bujanowicz <bujanowicz_kuba@interia.pl>
2024-02-09 17:03:46 +0100

    🔧 (package.json, astro.config.mjs): add ssr adapter for netlify

--- ip-tracker/astro.config.mjs
+++ ip-tracker/astro.config.mjs
@@ -1,9 +1,11 @@
 import { defineConfig } from 'astro/config';
-
 import qwikdev from "@qwikdev/astro";
+
+import netlify from "@astrojs/netlify";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [qwikdev()],
-  output: "server"
+  output: "server",
+  adapter: netlify()
 });
